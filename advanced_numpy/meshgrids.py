c527802bc60e84cb436f949c3030be3ac7cb3bc3
Saumy003 <saumysingh2005@gmail.com>
2026-05-19 16:28:09 +0530

    Topic 7. Meshgrids

--- advanced_numpy/meshgrids.py
+++ advanced_numpy/meshgrids.py
@@ -1,3 +1,32 @@
 """ Topic 7. Meshgrids """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
+#ploting 3D graphs
+
+#meshgrid
+a = np.linspace(-10, 9, 20)
+b = np.linspace(-10, 9, 20)
+
+xx, yy = np.meshgrid(a, b)
+
+
+plt.scatter(xx, yy)
+plt.show()
+
+
+def func(x, y):
+    return x**2 + y**2
+
+zz = func(xx, yy)
+
+
+# to print 3D graphs we are using plotly
+
+fig = px.scatter_3d()
+fig.add_trace(go.Surface(x = xx,y = yy, z = zz))
+
+fig.show()
